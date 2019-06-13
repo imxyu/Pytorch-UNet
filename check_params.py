7e5fc53c590f9abb801c0b3b16a624138fe6a584
xyu <tellxysth@gmail.com>
2019-06-14 00:45:13 +0800

    update

--- check_params.py
+++ check_params.py
@@ -2,6 +2,7 @@ from unet_parts import double_conv, double_dsconv
 from unet_model import UNet, UNet_dsc
 from torchsummary import summary
 import torch 
+from thop import profile
 
 model = UNet(3, 2)
 model_dsc = UNet_dsc(3, 2)
@@ -16,3 +17,8 @@ summary(conv_dsc.cuda(), (64, 512, 512))
 
 # torch.save(model.state_dict(), 'unet.pth')
 # torch.save(model_dsc.state_dict(), 'unet_dsc.pth')
+
+flops, params = profile(conv, input_size=(1, 64, 512, 512))
+print(flops, params)
+flops, params = profile(conv_dsc, input_size=(1, 64, 512, 512))
+print(flops, params)
